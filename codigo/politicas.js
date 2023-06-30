8c69acd5cd345ea61e54b7d4681402d48eec2ce0
Bac212 <82739072+Bac212@users.noreply.github.com>
2023-06-30 17:32:39 -0300

    UPLOAD POLITICAS

---
+++ codigo/politicas.js
@@ -0,0 +1,9 @@
+//VOLTAR PARA A PAGINA INICIAL
+
+const btnVoltar = document.getElementById("voltar");
+
+btnVoltar.addEventListener('click', function() {
+
+  window.location.href = 'index.html';
+
+});
